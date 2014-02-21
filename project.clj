202ba7a55c7ec1371b8f7b54c65b672253cd9fbc
Ray Miller <ray@metail.com>
2014-02-21 15:48:42 +0000

    Add --version flag to archive subcommand.

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject lein-immutant "1.2.1-SNAPSHOT"
+(defproject org.clojars.ray1729/lein-immutant "1.2.1-SNAPSHOT"
   :description "Leiningen plugin for managing an Immutant project."
-  :url "https://github.com/immutant/lein-immutant"
+  :url "https://github.com/ray1729/lein-immutant"
   :mailing-list {:name "Immutant users list"
                  :post "immutant-users@immutant.org"
                  :subscribe "immutant-users-subscribe@immutant.org"
@@ -11,7 +11,7 @@
   :eval-in-leiningen true
   :exclusions [org.clojure/clojure dynapath]
   :dependencies [[org.immutant/overlay                        "1.5.2"]
-                 [org.immutant/deploy-tools                   "0.14.0"]
+                 [org.clojars.ray1729/deploy-tools            "0.14.1-SNAPSHOT"]
                  [org.immutant/immutant-dependency-exclusions "0.1.0"]
                  [org.clojure/data.json                       "0.2.1"]
                  ;; [org.clojure/tools.cli                       "0.3.1"]
@@ -22,5 +22,5 @@
                              [midje      "1.3.1" :exclusions [org.clojure/core.unify]]]
               :resource-paths ["test-resources"]}}
   :pedantic :warn
-  :signing {:gpg-key "BFC757F9"}
+  :signing {:gpg-key "B009089F"}
   :lein-release {:deploy-via :clojars})
